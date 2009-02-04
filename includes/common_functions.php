073bffdd1ec23e58269196b9f9920d3d9969141b
Doycho <ironsmile@gmail.com>
2009-02-04 17:12:11 +0200

    html_post function

--- includes/common_functions.php
+++ includes/common_functions.php
@@ -46,4 +46,19 @@ function get_gravatar_src($email, $size = 100){
   "&default=".urlencode(DEFAULT_USER_PIC)."&size=".$size;
 }
 
+function urls_to_links( $string, $new_win = true ){
+  return preg_replace('@(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.-%]*(\?\S+)?)?)?)@', '<a href="$1"'.(($new_win)?" target='_blank'":"").'>$1</a>', $string);
+}
+
+function emails_to_links( $string ){
+  return preg_replace('@([\w\d-\._]{3,}\@[\w\d-\._]{3,}\.[\w\d-\._]{2,})@', '<a href="mailto:$1">$1</a>', $string);
+}
+
+function html_post( $string ){
+  $string = "<p>".str_replace("\n","</p>\n<p>",h($string))."</p>";
+  $string = urls_to_links($string);
+  $string = emails_to_links($string);
+  return preg_replace('/<p>\s*?<\/p>/', "", $string);
+}
+
 ?>
